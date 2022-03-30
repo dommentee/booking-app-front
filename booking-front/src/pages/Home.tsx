32572a9ac4926cdb2867de26cca9e39b9ce0ff52
dommentee <chuku0929@gmail.com>
2022-03-30 18:03:06 -0400

    added styling to home landing

--- booking-front/src/pages/Home.tsx
+++ booking-front/src/pages/Home.tsx
@@ -1,17 +1,21 @@
 import React from "react";
 import { useNavigate } from 'react-router-dom'
 
+import '../SASS/Home.scss';
+
 const Home = (props: any ) => {
     //navaget is used to route for different button 
 
 let navagate = useNavigate()
   return (
       <div className="home">
-          <div className="landing-banner"> 
+        <div className="background-pic"></div>
+        <div className="overlay">
+            
 
-          </div>
-
+        </div>
       </div>
+
   )
 }
 
