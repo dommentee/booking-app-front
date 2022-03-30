14850ac87a72112794a7923d7b26968b000351da
dommentee <chuku0929@gmail.com>
2022-03-29 20:30:33 -0400

    added main components

--- booking-front/src/App.tsx
+++ booking-front/src/App.tsx
@@ -1,26 +1,75 @@
 import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import { BrowserRouter as Router, Routes, Route, Link} from 'react-router-dom';
 
-function App() {
+//components 
+import Home from './pages/Home'
+import Signup from './pages/Signup';
+import Login from './pages/Login';
+import Footer from './components/Footer';
+
+
+
+const App = () => {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="container">
+      <Router>
+        <div className="header">
+          <div className="search-form-warp">
+           <Link to="/"><div className="home-button">SLayITKita</div></Link>
+            {/* <form onSubmit={handleSearch} className="seach-form">
+              <input 
+                type="input" onChange={(e: any) => setSearchInput(e.target.value)} 
+                className="searh-input"
+                placeholder="search procedure"
+                value={searchInput}
+                />
+              <input type="submit" value="search" className="submit-search" />
+            </form> */}
+          </div>
+          {/* <div className="user-wrap">
+            {
+              user ? (
+                <>                
+                  <Link to="/profile"><div className="session-button" id="login">profile</div></Link>
+                  <Link to="/"><div className="session-button" id="logout" onClick={logout}>logout</div></Link>
+                </>
+              ): (
+                <>
+                 <Link to="/signup"><div className="session-button" id="sign-up">signup</div></Link>
+                  <Link to="/login"><div className="session-button" id="login">login</div></Link>
+                </>
+              )
+            }
+          </div> */}
+        </div>
+        {/* {
+          searchResults ? (
+            <div className="results">
+              <h3>Search Results for {searchInput}</h3>
+              <div className="chartwrap">
+              </div> 
+              <div className="result-stats">
+              <h4>recommended hospitals/clinics</h4>
+            </div>
+            </div>
+          ): <></>
+        } */}
+        <Routes>
+          <Route path="/" element={
+            <Home/>
+          }/>
+          <Route path="/signup" element={<Signup />}/>
+          <Route path="/login" element={<Login/>}/>
+          {/* <Route path="/profile" element={<Profile
+           
+           />}/>
+          <Route path="*" element={<ErrorPage />} />  */}
+        </Routes>
+      </Router>
+      <Footer />
     </div>
-  );
+  )
+
 }
 
 export default App;
---
+++ booking-front/src/components/Footer.tsx
@@ -0,0 +1,14 @@
+import React from "react";
+
+const Footer = () => {
+    return (
+        <div className="footer">
+            <span className="name">SUR+GICAL</span>
+		    <div className="developer">
+			    <a href="https://domacor.netlify.app" target="_blank"> Domacor Mentee </a>
+		    </div>	
+
+        </div>
+    )
+}
+export default Footer
