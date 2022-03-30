e16ef8c7c1cc95933f1838e540373808beabfcf1
dommentee <chuku0929@gmail.com>
2022-03-30 18:03:33 -0400

    aded backgorund

--- booking-front/src/App.tsx
+++ booking-front/src/App.tsx
@@ -18,62 +18,24 @@ const App = () => {
       <Router>
         <div className="header">
           <div className="search-form-warp">
-           <Link to="/"><div className="home-button">SLayITKita</div></Link>
-            {/* <form onSubmit={handleSearch} className="seach-form">
-              <input 
-                type="input" onChange={(e: any) => setSearchInput(e.target.value)} 
-                className="searh-input"
-                placeholder="search procedure"
-                value={searchInput}
-                />
-              <input type="submit" value="search" className="submit-search" />
-            </form> */}
+            <Link to="/"><div className="home-button">SlayItKita</div></Link>
           </div>
-          {/* <div className="user-wrap">
-            {
-              user ? (
-                <>                
-                  <Link to="/profile"><div className="session-button" id="login">profile</div></Link>
-                  <Link to="/"><div className="session-button" id="logout" onClick={logout}>logout</div></Link>
-                </>
-              ): (
-                <>
-                  <Link to="/signup"><div className="session-button" id="sign-up">signup</div></Link>
-                  <Link to="/login"><div className="session-button" id="login">login</div></Link>
-                </>
-              )
-            }
-          </div> */}
-        <div className='user-wrap'>
-          <Link to="/signup"><div className="session-button" id="sign-up">signup</div></Link>
-          <Link to="/login"><div className="session-button" id="login">login</div></Link>
-        </div>
-        </div>
-        {/* {
-          searchResults ? (
-            <div className="results">
-              <h3>Search Results for {searchInput}</h3>
-              <div className="chartwrap">
-              </div> 
-              <div className="result-stats">
-              <h4>recommended hospitals/clinics</h4>
+            <div className='user-wrap'>
+
+              <a href='https://slayitkita.square.site/'className='book-now'>Book now</a>
+              {/* <Link to="/signup"><div className="session-button" id="sign-up">signup</div></Link> */}
+              {/* <Link to="/login"><div className="session-button" id="login">login</div></Link> */}
             </div>
-            </div>
-          ): <></>
-        } */}
+        </div>
         <Routes>
           <Route path="/" element={
             <Home/>
           }/>
           <Route path="/signup" element={<Signup />}/>
           <Route path="/login" element={<Login/>}/>
-          {/* <Route path="/profile" element={<Profile
-           
-           />}/>
-          <Route path="*" element={<ErrorPage />} />  */}
         </Routes>
       </Router>
-      <Footer />
+      {/* <Footer /> */}
     </div>
   )
 
