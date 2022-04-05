97de4728bd857358e3961c592451c0ac4752d956
dommentee <chuku0929@gmail.com>
2022-04-05 19:10:16 -0400

    added booking rule one

--- booking-front/src/components/BookingRules.tsx
+++ booking-front/src/components/BookingRules.tsx
@@ -9,6 +9,32 @@ const BookingRules = () => {
                 PLEASE READ ALL THE RULES BELOW BEORE BOOKING YOUR APPOINMENT.
                 THANK YOU!
             </p>
+            <div className='rule'>
+                <h3>PAYMENTS</h3>
+                <p>
+                    I  DO NOT ACCEPT CASH APP.
+                    CASH OR ZELLE ONLY.
+                    Deposits are Non-Refundalbe AND Non-Transferable
+                    Please stop at the bank before comming you can not 
+                    go to the bank after!
+                </p>
+            </div>
+            <div className='rule'>
+                <h3>SALON RULES</h3>
+                <p>
+                    
+                </p>
+
+            </div>
+            <div className='rule'>
+                <h3>PRE-CARE</h3>
+            </div>
+            <div className='rule'>
+                <h3>TIME MANAGEMENT</h3>
+            </div>
+            <div className='rule'>
+                <h3>MICROLINKS</h3>
+            </div>
         </div>
     )
 }
