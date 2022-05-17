5c655f1f7a9d93e92d8e8c57f8d8f9cce55df142
dommentee <chuku0929@gmail.com>
2022-05-17 18:51:16 -0400

    styled book buttons

--- booking-front/src/pages/Home.tsx
+++ booking-front/src/pages/Home.tsx
@@ -5,18 +5,19 @@ import '../SASS/Home.scss';
 
 const Home = (props: any ) => {
     //navaget is used to route for different button 
-
-let navagate = useNavigate()
+  let navagate = useNavigate()
   return (
-      <>
-        <div className="home">
-            <div className="background-pic"></div>
-            <div className="overlay">
-                <a href='https://slayitkita.square.site/'  target='_blank' className='book-now'>Book now</a>
-            </div>
-        
+    <>
+      <div className="home">
+        <div className="background-pic"></div>
+          <div className="overlay">
+            <a href='https://slayitkita.square.site/'  target='_blank' className='book-now'>Book now</a>
+          </div>        
         </div>
         <div className="banner">LICENSEd HAIR STYLIST</div>
+        <div className="hero">
+            
+        </div>
       </>
 
   )
